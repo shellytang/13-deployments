27f72b4cd485abce963315011fa90e3157359243
Shelly Tang <shellytang@gmail.com>
2017-01-19 17:14:37 -0800

    updated url

--- public/scripts/models/repo.js
+++ public/scripts/models/repo.js
@@ -7,7 +7,7 @@
 
   repos.requestRepos = function(callback) {
     // DONE: Refactor your ajax call to use the $.get method, and make a request to our new proxy route.
-    $.get('/github.com/user/repos?per_page=5&sort=updated')
+    $.get('/github/user/repos?per_page=5&sort=updated')
     .then(data => repos.all = data, err => console.error(err)) // es6 syntax arrow functions
     .then(callback);
   };
